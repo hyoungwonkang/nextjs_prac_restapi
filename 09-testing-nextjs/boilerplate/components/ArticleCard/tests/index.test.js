f26336d87c9e16a2bcc81c498942be6c0ebfc165
hyoungwonkang <hwplus@gmail.com>
2023-11-05 18:52:46 +0900

    Jest를 사용한 단위 테스트와 통합 테스트

---
+++ 09-testing-nextjs/boilerplate/components/ArticleCard/tests/index.test.js
@@ -0,0 +1,22 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from "@testing-library/react";
+import ArticleCard from "../index";
+import { cutTextToLength } from "../../../utils";
+import { article } from "../tests/mock";
+
+describe("ArticleCard", () => {
+  test("Generated link should be in the correct format", () => {
+    const component = render(<ArticleCard {...article} />);
+    const link = component.getByRole("link").getAttribute("href");
+    expect(link).toBe("/articles/healthy-summer-meloncarrot-soup-u12w3o0d");
+  });
+
+  test("Generated summary should not exceed 100 characters", async () => {
+    render(<ArticleCard {...article} />);
+    const summary = screen.getByText(cutTextToLength(article.body, 100));
+    expect(summary).toBeDefined();
+  });
+});
